2bc3158b129e06da0aeb00335ae94d77e5222009
seer-lee <2seer@naver.com>
2024-01-31 14:40:22 +0900

    fix: 22차시 수강

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -9,17 +9,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests()
-//                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-//                .requestMatchers("/notices", "/contact").permitAll()
-//                .and().formLogin()
-//                .and().httpBasic();
-
-//        http.authorizeHttpRequests().anyRequest().denyAll()
-//                .and().formLogin()
-//                .and().httpBasic();
-
-        http.authorizeHttpRequests().anyRequest().permitAll()
+        http.authorizeHttpRequests()
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                .requestMatchers("/notices", "/contact").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
 
