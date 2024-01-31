658b69f89d31cf4f0804dd4e0155968b8177c890
seer-lee <2seer@naver.com>
2024-01-31 12:54:40 +0900

    fix: 15차시 수강

--- src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -1,15 +1,15 @@
-package com.eazybytes.springsecuritybasic;
+package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.eazybytes.springsecuritybasic.controller") // Optional
-public class SpringsecuritybasicApplication {
+@ComponentScan("com.eazybytes.controller") // Optional
+public class EazyBankBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringsecuritybasicApplication.class, args);
+		SpringApplication.run(EazyBankBackendApplication.class, args);
 	}
 
 }
